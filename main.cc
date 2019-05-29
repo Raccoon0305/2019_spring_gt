c3f947ae561563c45344da2db82ac34dcd4bea97
Raccoon0305 <mercer0305@gmail.com>
2019-05-29 17:02:32 +0800

    fix some bugs

--- main.cc
+++ main.cc
@@ -6,28 +6,29 @@
 #include "gplot.h"
 #include "path.h"
 #include <vector>
+#include <map>
 #include <string>
 #include <queue>
 using namespace std;
 
 // create NetworkManager first
 NetworkManager *nm = new NetworkManager();
-NetworkManager *bicli = new NetworlManager();
+NetworkManager *bicli = new NetworkManager();
 int main(int argc, char** argv){
 
-    nm->intrepret(graph_in.txt);
+    nm->interpret("graph_in.txt");
     nm->print_all_e();
     nm->print_all_v();
 
 
     /*check every nodes, whether their in_degree=out_degree or not*/
-    Vertex *node = nm.get_all_nodes();
+    Vertex *node = nm->get_all_nodes();
     vector<Vertex *> node_vlist;
     vector<int> in_degree,out_degree;
     Vertex *current=node;
    
     while(current!=0){
-       node_vlist.pushback(current);
+       node_vlist.push_back(current);
        current=current->next;
        
     }
@@ -35,30 +36,30 @@ int main(int argc, char** argv){
     for(int i=0;i<node_vlist.size();i++){
        for(int j=0;j<node_vlist.size();j++){
           if(i!=j){
-             if(nm->connected_d(node_vlist[i]->name,node_vlist[j]->name){c_out_d++;}
-             if(nm->connected_d(node_vlist[j]->name,node_vlist[i]->name){c_in_d++;}   
+             if(nm->connected_d(node_vlist[i]->name,node_vlist[j]->name)){c_out_d++;}
+             if(nm->connected_d(node_vlist[j]->name,node_vlist[i]->name)){c_in_d++;}   
           }
        }
-       in_degree.pushback(c_in_d);
-       out_degree.pushback(c_out_d);
+       in_degree.push_back(c_in_d);
+       out_degree.push_back(c_out_d);
        c_in_d=0;
        c_out_d=0;
     }
     vector<string> outer,inter;
     for(int i=0;i!=in_degree.size();i++){
-       if(in_degree.at(i)>out_degree.at(i)){inter.pushback(node_vlist.at(i)->name);}
-       else if(in_degree.at(i)<out_degree.at(i)){outer.pushback(node_vlist.at(i)->name);}
+       if(in_degree.at(i)>out_degree.at(i)){inter.push_back(node_vlist.at(i)->name);}
+       else if(in_degree.at(i)<out_degree.at(i)){outer.push_back(node_vlist.at(i)->name);}
     }
  
 //Build a biclique graph which all the edges of it present the minimun length from a inter(in_degree>out_degree) to an 
 //outer(out_degree>in_degree).
     
     Path *cur_path=new Path();
-    cur_path.append(nm->elist);
+    cur_path->append(nm->elist);
     vector<vector<Edge *>> cur_paths;
     map<Edge *,vector<Edge *>> choosed_paths;
-    vector<Edge *>::iterator min_path;
-    float  cur_lentgh=0;
+    int min_path;
+    float  cur_length=0;
     float  min_len=10000;
        
     if(inter.size()!=outer.size()){
@@ -69,30 +70,30 @@ int main(int argc, char** argv){
 //find the shortest path from each inter to each outer.
       for(int i=0;i<inter.size();i++){
          for(int j=0;j<outer.size();j++){
-           cur_paths= cur_path.find_path(inter.at(i),outer.at(j));
-           cur_path.debug();
-           if(curpaths.at(0).size==0){
+           cur_paths= cur_path->find_paths(inter.at(i),outer.at(j));
+           cur_path->debug();
+           if(cur_paths.at(0).size()==0){
               cout<<"This graph is not strongly connected"<<endl;
               return 0; //checker
            } 
-           for(vector<vector<Edge *>>::iterator it_i=cur_paths.begin();it_i!=cur_paths.end();it_i++){
-              for(vector<Edge *>::iterator i=it_i.begin();i!=it_i.end();i++){
-                 cur_length=cur_length +(i->flowval);
+           for(int k=0;k<cur_paths.size();k++){
+              for(int l=0;l<cur_paths.at(k).size();l++){
+                 cur_length=cur_length +(cur_paths.at(k).at(l))->flowval;
               }
               if(cur_length<min_len){
                  min_len=cur_length;
-                 min_path=it_i
+                 min_path=k;
               }
            }
            
            //as find the shortest path, add an edge which its weight(flowval)=the sum of the edges' weight
            bicli->add_switch(inter.at(i));
-           blcli->add_switch(outer.at(j));
+           bicli->add_switch(outer.at(j));
            bicli->connect(inter.at(i),outer.at(j));
            bicli->setlink(inter.at(i),outer.at(j),1,min_len);
-           Edge *e= bicli->get_edge(inter.at(i),outer(j));
+           Edge *e= bicli->get_edge(inter.at(i),outer.at(j));
            e->tag="notused";//mark as notused, will be used later in matching part
-           choosed_paths.insert(pair<Edge *,vector<Edge *>>(e,*min_path))//record the relationship between edge in biclique and the orinal path
+           choosed_paths.insert(pair<Edge *,vector<Edge *>>(e,cur_paths.at(min_path)));//record the relationship between edge in biclique and the orinal path
            cur_length=0;
            min_len=10000;
              
@@ -103,22 +104,22 @@ int main(int argc, char** argv){
    Edge *c_edge = bicli->elist;
    Edge *min_edge = c_edge;
    vector<Edge *> choosed_edges;
-  // vector<Vertex *> matched_v;
-   while(!inter.empty&& !outer.empty){
+   vector<string> matched_v;
+   while(!inter.empty()&& !outer.empty()){
     while(c_edge!=0){
-       if((c_edge->flowval < min_edge->flowval)&&(c_edge->tag=="notused")&&(find(inter.begin(),inter.end(),c_edge->head->name)!=inter.end())&&(find(outer.begin(),outer.end(),c_edge->tail->name)!=inter.end()))
+       if((c_edge->flowval < min_edge->flowval)&&(c_edge->tag=="notused")&&!(find(matched_v.begin(),matched_v.end(),c_edge->head->name)!=matched_v.end())&&!(find(matched_v.begin(),matched_v.end(),c_edge->tail->name)!=matched_v.end()))
         {
             min_edge=c_edge;//greedy,find the edge with smallest value and connect 2 unmatched vertices.
         } 
     }
-    choosed_edges.pushback(min_edge);
-    inter.erase(min_edge->head->name);
-    outer.erase(min_edge->tail->name);//erase from vectors for recording
+    choosed_edges.push_back(min_edge);
+    matched_v.push_back(min_edge->head->name);
+    matched_v.push_back(min_edge->tail->name);//Record matched vertex in the matched_v
    }
    
-   for(vector<Edge *>::iterator it_e=choosed_edges.begin();it_e!=choosed_edges.end();it_e++){
-	      for(vector<Edge *>::iterater p_it = choosed_paths[it_e].begin();p_it != choosed_paths[it_e].end();p_it++){
-		 nm->add_edge(p_it);//copy edges which will be walked twice to nm
+   for(int i=0;i<choosed_edges.size();i++){
+	      for(int j=0;j<choosed_paths[choosed_edges.at(i)].size();j++){
+		 nm->add_edge(choosed_paths[choosed_edges.at(i)].at(j));//copy edges which will be walked twice to nm
       }
    }
    
